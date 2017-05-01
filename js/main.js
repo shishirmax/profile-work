a8cdf03bff04896ccbf96fd3aa278024530142c6
Shishir <shishirmax@gmail.com>
2017-05-01 18:37:48 +0530

    new functionality
    wow and animate

--- js/main.js
+++ js/main.js
@@ -4,4 +4,18 @@ $(document).ready(function($){
 		$(".preloader").delay(1000).fadeOut("slow");
 	});
 
+	var amountScrolled = 500;
+	$(window).scroll(function(){
+		if($(window).scrollTop()>amountScrolled){
+			$('a.back-to-top').fadeIn('slow');
+		}else{
+			$('a.back-to-top').fadeOut('slow');
+		}
+	});
+	$('a.back-to-top').click(function(){
+		$('html,body').animate({
+			scrollTop:0
+		},700);
+		return false;
+	});
 });
